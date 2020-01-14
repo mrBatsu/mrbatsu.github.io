a8a2162f2894881ad5ee7eed6d218a39ead40a22
maique madeira <maique@xanato.net>
2020-01-14 13:21:52 +0000

    Update scripts.js

--- strt/scripts.js
+++ strt/scripts.js
@@ -1,4 +1,4 @@
-const defaultsites = "d s stackexchange https://stackexchange.com/ https://stackexchange.com/search?q=,m r reddit https://www.reddit.com/ https://www.reddit.com/search/?q=,m t twitter https://twitter.com/home https://twitter.com/search?q=,w w wikipedia https://www.wikipedia.org/ https://en.wikipedia.org/wiki/,x s stellar http://coinbase.com/price/stellar, x b bitwarden https://vault.bitwarden.com/#/tools/generator, x w https://www.waze.com/en-GB/livemap";
+const defaultsites = "d s stackexchange https://stackexchange.com/ https://stackexchange.com/search?q=,m r reddit https://www.reddit.com/ https://www.reddit.com/search/?q=,m t twitter https://twitter.com/home https://twitter.com/search?q=,w w wikipedia https://www.wikipedia.org/ https://en.wikipedia.org/wiki/,x s stellar http://coinbase.com/price/stellar,x b bitwarden https://vault.bitwarden.com/#/tools/generator,x w https://www.waze.com/en-GB/livemap";
 const searchengine = "https://www.duckduckgo.com/search?q=";
 const siteFile = "strtsites";
 
